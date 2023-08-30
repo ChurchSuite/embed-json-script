0a9e60610ee2808a6f09e95423bf360a2dc5c851
Ant Weedon <ant@churchsuite.com>
2023-08-30 17:06:06 +0100

    Add a test to expose bug in Organisation label filtering

---
+++ __tests__/network/CSOrganisation.test.js
@@ -0,0 +1,56 @@
+import CSOrganisations from '../../src/network/CSOrganisations';
+
+/**
+ * Set up dependencies on the virtual window
+ */
+var dayjs = require('dayjs')
+
+// require isoweek to create dayjs objects for days
+var isoWeek = require('dayjs/plugin/isoWeek')
+dayjs.extend(isoWeek)
+window.dayjs = dayjs
+
+window.CS = {url: 'https://demo.churchsuite.com'};
+
+/**
+ * Label filtering should be an OR check rather than AND - if two labels are
+ * selected, any organisation that is one OR the other should be returned.
+ */
+test('label filtering', () => {
+	let CSO = new CSOrganisations;
+	let model = {
+		labels: [
+			{
+				id: 1, // label id 1
+				value: ['a'], // label options
+			},
+			{
+				id: 2,
+				value: ['b', 'c'],
+			},
+		]
+	}
+
+	// option C has been selected for label 2
+	CSO.label = {
+		'2': ['c']
+	}
+
+	// the model has label 2 with a value of c, should be fine
+	expect(CSO.filterModel_Label(model)).toBe(true);
+
+	// now we've selected a second filter - should still be fine though
+	CSO.label = {
+		'2': ['c'],
+		'3': ['d'],
+	}
+
+	expect(CSO.filterModel_Label(model)).toBe(true);
+
+	// finally, we've only selected one this model doesn't have
+	CSO.label = {
+		'3': ['d'],
+	}
+
+	expect(CSO.filterModel_Label(model)).toBe(false);
+});
