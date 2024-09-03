8cdd7dced0a1233b1ac8c10e6320100150bf3562
Ant Weedon <ant@churchsuite.com>
2024-09-03 14:44:42 +0100

    Dynamically fill in the package version
    I keep forgetting to do this, so we can be cleverer!

--- src/cs.js
+++ src/cs.js
@@ -43,7 +43,7 @@ window.CSGroups = CSGroups
 import CSOrganisations from './network/CSOrganisations'
 window.CSOrganisations = CSOrganisations
 
-let scriptVersion = '4.3.4'
+let scriptVersion = '@package_version@'
 
 // our main json feed object
 window.CS = {
