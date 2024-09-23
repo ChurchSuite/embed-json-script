2937ff073e2401760762faad4186f359c88abb3f
Ant Weedon <ant@churchsuite.com>
2024-09-23 15:58:53 +0100

    Fix indentation

--- src/calendar/configuration.js
+++ src/calendar/configuration.js
@@ -4,19 +4,19 @@ export default class Configuration {
 	 * @param {object} json
 	 */
 	constructor(json) {
-        this.uuid = json.uuid,
-        this.brand_id = json.brand_id,
-        this.name = json.name,
-        this.description = json.description,
-        this.filterByCategory = json.filter_by_categories,
-        this.filterByFeatured = json.filter_by_featured,
-        this.filterBySite = json.filter_by_sites,
-        this.format = json.format,
-        this.layout = json.layout,
-        this.mergeEvents = json.merge_events,
-        this.numOfEvents = json.num_events,
-        this.numOfMonths = json.num_months,
-        this.showFilters = json.show_filters,
-        this.weekStartDay = json.week_start_day
+		this.uuid = json.uuid,
+		this.brand_id = json.brand_id,
+		this.name = json.name,
+		this.description = json.description,
+		this.filterByCategory = json.filter_by_categories,
+		this.filterByFeatured = json.filter_by_featured,
+		this.filterBySite = json.filter_by_sites,
+		this.format = json.format,
+		this.layout = json.layout,
+		this.mergeEvents = json.merge_events,
+		this.numOfEvents = json.num_events,
+		this.numOfMonths = json.num_months,
+		this.showFilters = json.show_filters,
+		this.weekStartDay = json.week_start_day
 	}
 }
